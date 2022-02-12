054cdad3e8cdb20a6519e2486e32d18e588d7e7e
J-YAA-i50-L <97762136+J-YAA-i50-L@users.noreply.github.com>
2022-02-12 21:53:34 +0300

    Add files via upload

---
+++ obmen_primer_py.py
@@ -0,0 +1,26 @@
+import time
+import serial
+import random
+
+ser = serial.Serial('COM7', 9600)
+time.sleep(2)
+ser.reset_input_buffer()
+Time = 1000
+while True:
+    try:
+        ser_bytes = ser.readline()  # Uncomment this three lines if you want to read info from photoresistor
+        decoded_bytes = float(ser_bytes[0:len(ser_bytes) - 2].decode("utf-8"))
+        R, G, B = 0, 0 ,0
+        print(decoded_bytes)
+        if decoded_bytes == 1:
+            R, G, B = random.randrange(0, 2), random.randrange(0, 2), random.randrange(0, 2)
+            #Time = random(1000, 2001)
+            D1 = str(R) + "," + str(G) + "," + str(B) + "," + str(Time)
+            print(D1)
+            b1 = bytes(D1, encoding='utf-8')
+            ser.write(b1)
+        #print(R, G, B, Time)
+               
+    except:
+        print("Keyboard Interrupt")
+        break
